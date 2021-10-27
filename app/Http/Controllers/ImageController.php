e69739814af5af6e09b4b7ac466f7538992963fb
jssaninv <jssaninv@eafit.edu.co>
2021-10-26 20:37:33 -0500

    Imagenes

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,30 +6,33 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class ImageController extends BaseController
 {
-    public static $quotes = array("https://storage.googleapis.com/imagenes_taller2/imagen1.jpg", 
-        "https://storage.googleapis.com/imagenes_taller2/imagen10.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen11.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen12.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen13.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen14.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen15.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen2.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen3.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen4.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen5.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen6.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen7.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen8.jpg",
-        "https://storage.googleapis.com/imagenes_taller2/imagen9.jpg",
-    );
+    public static $images = array("https://storage.googleapis.com/imagenes_taller2/imagen1.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen2.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen3.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen4.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen5.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen6.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen7.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen8.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen9.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen10.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen11.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen12.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen13.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen14.jpg",
+    "https://storage.googleapis.com/imagenes_taller2/imagen15.jpg",
+);
 
-    public function image()
-    {
-        $totalQuotes = (count(Controller::$quotes));
-        $randomNumber = (rand(0,($totalQuotes-1)));
-        $randomQuote = Controller::$quotes[$randomNumber];
-        return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
-    }
+public function image()
+{
+    $numberOfImages = 15;
+    $randomNumber = (rand(0, ($numberOfImages-1)));
+    $randomImage = Controller::$images[$randomNumber];
+    $type = 'image/jpg';
+    $headers = ['Content-Type' => $type];
+    $response = new BinaryFileResponse($randomImage, 200, $headers);
+    return $response;
+}
 
     
 }
